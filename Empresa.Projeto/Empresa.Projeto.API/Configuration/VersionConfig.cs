1540ac3c3609f2981f11bae8aed91b9481f94c3e
Paulo Diego <paulo.diego@adm.unip.br>
2022-01-20 20:41:19 -0300

    Otimização do CorsConfig e VersionConfig

---
+++ Empresa.Projeto/Empresa.Projeto.API/Configuration/VersionConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Empresa.Projeto.API
+{
+    public static class VersionConfig 
+    {
+        public static void AddVersionConfiguration(this IServiceCollection services)
+        {
+            services.AddApiVersioning(o =>
+            {
+                o.UseApiBehavior = false;
+                o.ReportApiVersions = true;
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.DefaultApiVersion = new ApiVersion(1, 0);
+                o.ApiVersionReader = ApiVersionReader.Combine(
+                    //new HeaderApiVersionReader("x-api-version"),
+                    //new QueryStringApiVersionReader(),
+                    new UrlSegmentApiVersionReader());
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+        }
+    }
+}
--- Empresa.Projeto/Empresa.Projeto.API/Startup.cs
+++ Empresa.Projeto/Empresa.Projeto.API/Startup.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,85 +18,28 @@ namespace Empresa.Projeto.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
             services.AddJwtTConfiguration(Configuration);
-
             services.AddFluentValidationConfiguration();
-
             services.AddAutoMapperConfiguration();
-
             services.AddDatabaseConfiguration(Configuration);
-
             services.AddDependencyInjectionConfiguration();
-
             services.AddSwaggerConfiguration();
-
-            #region :: CORS ::
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy", builder =>
-                {
-                    var origins = Configuration.GetValue<string>("CorsOrigins").Split(";");
-                    builder
-                        .WithOrigins(origins)
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
-                });
-            });
-
-            #endregion :: CORS ::
-
-            #region :: Versionamento ::
-
-            services.AddApiVersioning(o =>
-            {
-                o.UseApiBehavior = false;
-                o.ReportApiVersions = true;
-                o.AssumeDefaultVersionWhenUnspecified = true;
-                o.DefaultApiVersion = new ApiVersion(1, 0);
-
-                o.ApiVersionReader = ApiVersionReader.Combine(
-                    //new HeaderApiVersionReader("x-api-version"),
-                    //new QueryStringApiVersionReader(),
-                    new UrlSegmentApiVersionReader());
-            });
-
-            services.AddVersionedApiExplorer(options =>
-            {
-                options.GroupNameFormat = "'v'VVV";
-                options.SubstituteApiVersionInUrl = true;
-            });
-
-            #endregion
-
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Empresa.Projeto.API", Version = "v1" });
-            //});
+            services.AddCorsConfiguration(Configuration);
+            services.AddVersionConfiguration();             
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Empresa.Projeto.API v1"));
+                app.UseDeveloperExceptionPage();               
             }
 
             app.UseDatabaseConfiguration();
-
             app.UseSwaggerConfiguration(env);
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseCors("CorsPolicy");
-
-            //app.UseJwtConfiguration();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
