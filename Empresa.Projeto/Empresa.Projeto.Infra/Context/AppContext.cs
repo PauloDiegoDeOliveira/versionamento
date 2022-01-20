c599b494808b1ea8dc4b14977dcd44eddd7a5657
Paulo Diego <paulo.diego@adm.unip.br>
2022-01-20 16:56:26 -0300

    Removido migration

--- Empresa.Projeto/Empresa.Projeto.Infra/Context/AppContext.cs
+++ Empresa.Projeto/Empresa.Projeto.Infra/Context/AppContext.cs
@@ -5,21 +5,12 @@ namespace Empresa.Projeto.Infra
 {
     public class AppContext : DbContext
     {
-        //// Migration
-        //public AppContext() { } 
-
         public AppContext(DbContextOptions<AppContext> options) : base(options)
         {
 
         }
 
         public DbSet<Usuario> Usuarios { get; set; }
-
-        //// Migration
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer("Data Source=Diego-PC;Initial Catalog=Projeto;Integrated Security=SSPI;");
-        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
