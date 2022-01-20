730edf336801fb11b2a78e88244c50c49519f387
Paulo Diego <paulo.diego@adm.unip.br>
2022-01-20 12:56:52 -0300

    Entidade usuario

---
+++ Empresa.Projeto/Empresa.Projeto.Domain/Entities/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Empresa.Projeto.Domain
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }     
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Apelido { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }   
+        public int Status { get; set; }
+        public DateTime? CriadoEm { get; set; }
+        public DateTime? AlteradoEm { get; set; }
+    }
+}
