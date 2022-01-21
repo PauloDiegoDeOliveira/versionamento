a5c50dcc1b28536f1a8b4ce5d434eb4e57607b6a
Paulo Diego <paulo.diego@adm.unip.br>
2022-01-21 18:46:32 -0300

    HashPassword

--- Empresa.Projeto/Empresa.Projeto.Service/Services/UsuarioService.cs
+++ Empresa.Projeto/Empresa.Projeto.Service/Services/UsuarioService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Empresa.Projeto.Domain;
 using Empresa.Projeto.Infra;
+using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -32,23 +33,39 @@ namespace Empresa.Projeto.Service
 
         public async Task<ViewUsuario> PostAsync(PostUsuario post)
         {
+            ConverteSenhaEmHash(post);
+
             var consulta = mapper.Map<Usuario>(post);
             consulta = await usuarioRepository.PostAsync(consulta);
             return mapper.Map<ViewUsuario>(consulta);
         }
 
+        private static void ConverteSenhaEmHash(PostUsuario post)
+        {
+            var passwordHasher = new PasswordHasher<PostUsuario>();
+            post.Senha = passwordHasher.HashPassword(post, post.Senha);
+        }
+
         public async Task<ViewUsuario> PutAsync(PutUsuario put)
         {
+            ConverteSenhaEmHash(put);
+
             var consulta = mapper.Map<Usuario>(put);
             consulta = await usuarioRepository.PutAsync(consulta);
             return mapper.Map<ViewUsuario>(consulta);
         }
 
-        public async Task<ViewUsuario> DeleteAsync(int id) 
+        private static void ConverteSenhaEmHash(PutUsuario put)
+        {
+            var passwordHasher = new PasswordHasher<PutUsuario>();
+            put.Senha = passwordHasher.HashPassword(put, put.Senha);
+        }
+
+        public async Task<ViewUsuario> DeleteAsync(int id)
         {
             var consulta = await usuarioRepository.DeleteAsync(id);
             return mapper.Map<ViewUsuario>(consulta);
-        }       
+        }
 
         public async Task<List<ViewUsuario>> GetBuscarNomeAsync(string nome)
         {
