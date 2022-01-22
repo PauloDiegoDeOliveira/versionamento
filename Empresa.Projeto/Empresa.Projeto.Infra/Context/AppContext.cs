6754e7c1c467e740e4aeeea21360576882edde1d
Paulo Diego <paulo.diego@adm.unip.br>
2022-01-21 23:13:23 -0300

    EntityState.Modified criadoEm

--- Empresa.Projeto/Empresa.Projeto.Infra/Context/AppContext.cs
+++ Empresa.Projeto/Empresa.Projeto.Infra/Context/AppContext.cs
@@ -1,21 +1,35 @@
 ﻿using Empresa.Projeto.Domain;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Empresa.Projeto.Infra
 {
     public class AppContext : DbContext
     {
-        public AppContext(DbContextOptions<AppContext> options) : base(options)
-        {
+        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
 
-        }
-
-        public DbSet<Usuario> Usuarios { get; set; }       
+        public DbSet<Usuario> Usuarios { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("CriadoEm") != null))
+            {   
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("CriadoEm").IsModified = false;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
